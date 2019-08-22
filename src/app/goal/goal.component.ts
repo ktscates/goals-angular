92011a30ca722174f292ca3defd89beaf06d179d
ktscates <cates2506@gmail.com>
2019-08-22 15:03:33 +0200

    Second commit

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Goal } from '../goal';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-goal',
@@ -24,6 +25,22 @@ export class GoalComponent implements OnInit {
     if(isComplete){
       this.goals.splice(index,1);
     }
+  }
+  deleteGoal(isComplete, index){
+    if(isComplete){
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+
+      if(toDelete){
+        this.goals.splice(index,1)
+      }
+    }
+  }
+
+  addNewGoal(goal){
+    let goalLength = this.goals.length;
+    goal.id = goalLength+1;
+    goal.completeDate = new Date(goal.completeDate)
+    this.goals.push(goal);
   }
   constructor() { }
 
